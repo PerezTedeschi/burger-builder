6b044dbf85e50e5b1a67e21c88a66df570c1c55f
Perez Tedeschi, Santiago <santiagopereztedeschi@hotmail.com>
2020-08-07 15:47:13 -0300

    Added button component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -72,6 +72,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
 
+  purchaseContinueHandler = () => {
+    alert('Continue');
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients,
@@ -83,8 +87,16 @@ class BurgerBuilder extends Component {
 
     return (
       <EnclosingWrapper>
-        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients} />
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}            
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
