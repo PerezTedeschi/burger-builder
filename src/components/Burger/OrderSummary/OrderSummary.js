4991a85704546cc84d1d02511ee932d10a3b5979
Perez Tedeschi, Santiago <santiagopereztedeschi@hotmail.com>
2020-08-14 19:48:17 -0300

    Added order buttons

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import EnclosingWrapper from '../../../hoc/EnclosingWrapper';
 
-import Button from '../../UI/Button/Button'
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((key) => {
@@ -18,10 +18,16 @@ const orderSummary = (props) => {
       <h3>Your order</h3>
       <p>A delicious burger with following ingredients:</p>
       <ul>{ingredientSummary}</ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </EnclosingWrapper>
   );
 };
